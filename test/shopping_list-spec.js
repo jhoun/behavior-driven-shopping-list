048acfd5f48476b5625e90bdb27a2b66d5f27a8e
jhoun <jayhoun@gmail.com>
2016-12-20 17:14:23 -1000

    name property for ShoppingListItem failed

--- test/shopping_list-spec.js
+++ test/shopping_list-spec.js
@@ -3,10 +3,24 @@ const expect = chai.expect;
 chai.should();
 
 describe('ShoppingListItem', () => {
+
+  let myShoppingListItem;
+
+  beforeEach(function () {
+    myShoppingListItem = new ShoppingListItem("iphone");
+  });
+
   it('should be a class', () => {
     ShoppingListItem.should.be.a("function");
   });
 
+
+  describe('constructor()', () => {
+
+  })
+  it('should have a name property', () => {
+    expect(myShoppingListItem).to.have.property('');
+  })
   // let shoppingListItem;
   // beforeEach(() => {
   //   shoppingListItem = new ShoppingListItem('iphone 7s');
